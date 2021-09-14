ace612ca24eeab851ac2c66db45a3fac3fa17a57
Sarmad-Chandio <87301967+Sarmad-Chandio@users.noreply.github.com>
2021-09-14 11:33:38 +0500

    Update Post.js
    Axios with useState and useEffect hooks

--- Assignment # 04/src/components/Post/Post.js
+++ Assignment # 04/src/components/Post/Post.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios'
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
+
+//https://jsonplaceholder.typicode.com/posts
+
+const post = () => {
+    const[posts,setPosts]=useState([])
+
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response=>{
+            console.log(response)
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    })
+   
+
+    return(
+        <div>
+            <ul>
+                {
+                    posts.map(post=>
+                    <li key={post.id}>
+                        {post.title}
+                    </li>)
+                }
+            </ul>
+            <article className="Post">
+                <h1>Title</h1>
+                <div className="Info">
+                    <div className="Author">Author</div>
+                </div>
+            </article>
         </div>
-    </article>
-);
+    )
+    }
 
 export default post;
